1ea95428909ed4d1455b48658b350acd29ec7a77
chaemin <w2114@e-mirim.hs.kr>
2022-12-03 18:46:33 +0900

    오류 수정

--- js/index2.js
+++ js/index2.js
@@ -1,5 +1,4 @@
 
-<<<<<<< HEAD
 const url =`https://api.odcloud.kr/api/3084473/v1/uddi:9b55d736-6ab0-468a-9436-da76a357b49a_201910181439?page=1&perPage=30&serviceKey=dEBxo%2Bz1i1TcE5lcjM42Ko1CmlwFhQI8rsP0khF4uaoB0J2z7BdeIqWUbDkchzcIIRFbezp6Bfy3Jc48kwukDg%3D%3D`;
 
 for(let i=0; i<17; i++){
@@ -18,26 +17,5 @@ for(let i=0; i<17; i++){
         second.innerHTML = 주소;
 
     })
-=======
-
-for(let i = 0; i < 1; i++){
-    const url =`https://api.odcloud.kr/api/3084473/v1/uddi:9b55d736-6ab0-468a-9436-da76a357b49a_201910181439?page=`+i+`&perPage=17&serviceKey=dEBxo%2Bz1i1TcE5lcjM42Ko1CmlwFhQI8rsP0khF4uaoB0J2z7BdeIqWUbDkchzcIIRFbezp6Bfy3Jc48kwukDg%3D%3D`;
-        for(let j=0; j<17; j++){
-            fetch(url)
-            .then(res => res.json())
-            .then(myJson => {
-                const obj= myJson.data[j];
-                const 시설명 = obj.시설명.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-                const 주소 = obj.주소.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-                // console.log(시설명);
-                let menus1 = document.querySelectorAll(".card-menu1");
-                let menus2 = document.querySelectorAll(".card-menu2")
-                let first = menus1[j];
-                let second = menus2[j];
-                first.innerHTML = 시설명;
-                second.innerHTML = 주소;
-            })
-    }
->>>>>>> 978ada985d41ce5a2792ca075aa4a2de0af1e19f
 }
 
