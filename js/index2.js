b9733a4ef356cbbbd736d9e8ab70893552b3b735
Z0ro0 <chlrkdmf0805@gmail.com>
2022-12-03 18:01:33 +0900

    api 최종

--- js/index2.js
+++ js/index2.js
@@ -1,23 +1,22 @@
 
-const url =`https://api.odcloud.kr/api/3084473/v1/uddi:9b55d736-6ab0-468a-9436-da76a357b49a_201910181439?page=1&perPage=10&serviceKey=dEBxo%2Bz1i1TcE5lcjM42Ko1CmlwFhQI8rsP0khF4uaoB0J2z7BdeIqWUbDkchzcIIRFbezp6Bfy3Jc48kwukDg%3D%3D`;
 
-for(let i=0; i<9; i++){
-    fetch(url)
-    .then(res => res.json())
-    .then(myJson => {
-        const obj= myJson.data[i];
-        const 시설명 = obj.시설명.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        const 주소 = obj.주소.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        // console.log(시설명);
-        let menus1 = document.querySelectorAll(".card-menu1");
-        let menus2 = document.querySelectorAll(".card-menu2")
-        let first = menus1[i];
-        let second = menus2[i];
-        first.innerHTML = 시설명;
-        second.innerHTML = 주소;
-
-    })
+for(let i = 0; i < 1; i++){
+    const url =`https://api.odcloud.kr/api/3084473/v1/uddi:9b55d736-6ab0-468a-9436-da76a357b49a_201910181439?page=`+i+`&perPage=17&serviceKey=dEBxo%2Bz1i1TcE5lcjM42Ko1CmlwFhQI8rsP0khF4uaoB0J2z7BdeIqWUbDkchzcIIRFbezp6Bfy3Jc48kwukDg%3D%3D`;
+        for(let j=0; j<17; j++){
+            fetch(url)
+            .then(res => res.json())
+            .then(myJson => {
+                const obj= myJson.data[j];
+                const 시설명 = obj.시설명.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+                const 주소 = obj.주소.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+                // console.log(시설명);
+                let menus1 = document.querySelectorAll(".card-menu1");
+                let menus2 = document.querySelectorAll(".card-menu2")
+                let first = menus1[j];
+                let second = menus2[j];
+                first.innerHTML = 시설명;
+                second.innerHTML = 주소;
+            })
+    }
 }
-
-//Javascript
 
