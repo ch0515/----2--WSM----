0cc51f86c20ea94367556f1e69b995edad71ced3
chaemin <w2114@e-mirim.hs.kr>
2022-12-03 18:41:55 +0900

    api 수정 , index.html 첫번째 상품 페이지 연결

--- js/index2.js
+++ js/index2.js
@@ -1,7 +1,7 @@
 
-const url =`https://api.odcloud.kr/api/3084473/v1/uddi:9b55d736-6ab0-468a-9436-da76a357b49a_201910181439?page=1&perPage=10&serviceKey=dEBxo%2Bz1i1TcE5lcjM42Ko1CmlwFhQI8rsP0khF4uaoB0J2z7BdeIqWUbDkchzcIIRFbezp6Bfy3Jc48kwukDg%3D%3D`;
+const url =`https://api.odcloud.kr/api/3084473/v1/uddi:9b55d736-6ab0-468a-9436-da76a357b49a_201910181439?page=1&perPage=30&serviceKey=dEBxo%2Bz1i1TcE5lcjM42Ko1CmlwFhQI8rsP0khF4uaoB0J2z7BdeIqWUbDkchzcIIRFbezp6Bfy3Jc48kwukDg%3D%3D`;
 
-for(let i=0; i<9; i++){
+for(let i=0; i<17; i++){
     fetch(url)
     .then(res => res.json())
     .then(myJson => {
